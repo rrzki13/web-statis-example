eb5fea9132b2b5a63546b1cf7af4f39ab7adcc19
Rizki Ramadhan <farazrizki13@gmail.com>
2020-10-14 18:48:36 +0700

    mengedit regist.html

--- js/function.js
+++ js/function.js
@@ -400,6 +400,7 @@ function ValidateEmail(r) {
 
 function justText(r) {
   const id = r.getAttribute("id");
+  alert(id);
   if (/^[a-z A-Z]*$/.test(r.value)) {
     get("#" + id + "TextValidate").innerHTML = "";
     return true;
