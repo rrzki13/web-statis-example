b12a121d886c807fcd0e677f13b91276eac53b2f
Rizki Ramadhan <farazrizki13@gmail.com>
2020-10-15 19:20:48 +0700

    menyelesaikan history proses

--- js/history.js
+++ js/history.js
@@ -39,27 +39,112 @@ for (let i = 0; i < lihatTiket.length; i++) {
   lihatTiket[i].addEventListener("click", function () {
     get(".blank").style.opacity = 1;
     get(".blank").style.pointerEvents = "visible";
+    get("#theTiketHistory").style.opacity = 1;
+    get("#theTiketHistory").style.pointerEvents = "visible";
+
+    let loading = /* html */ `
+    <div class="row justify-content-center" style="margin-top: -20rem;">
+    <div class="col-md-11">
+      <div class="card">
+        <div class="card-body d-inline">
+          <div class="row">
+            <div class="col-3">
+              <img src="img/833.gif" width="100" class="mr-3" />
+            </div>
+            <div class="col-7 ml-3">
+              <h2 class="mt-4">Loading your ticket</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
+    let string = /* html */ `
+    <div class="card-tiket shadow">
+            <div class="headTiket"></div>
+            <div class="bodyTiket user-select-none">
+              <div class="container">
+                <div class="row">
+                  <div class="col-6 mt-2">
+                    <img src="img/kai_2.png" width="50" />
+                  </div>
+                  <div class="col-6 mt-2 text-center h5">
+                    BOARDING PASS
+                  </div>
+                </div>
+                <div class="row mt-2">
+                  <div class="col-6">
+                    <div class="row overflow-hidden" style="max-height: 50px;">
+                      <div class="col-6">
+                        <small>nama / name</small><br />
+                        <small class="font-weight-bold">Rizki Ramadhan</small>
+                      </div>
+                      <div class="col-6">
+                        <small>no. pesanan</small><br />
+                        <small class="font-weight-bold">160259771197911</small>
+                      </div>
+                    </div>
+                    <div class="row overflow-hidden" style="max-height: 50px;">
+                      <div class="col-6">
+                        <small>kereta / train</small><br />
+                        <small class="font-weight-bold">Argo Parahyangan</small>
+                      </div>
+                      <div class="col-6">
+                        <small>kelas / class</small><br />
+                        <small class="font-weight-bold">Ekonomi</small>
+                      </div>
+                    </div>
+                    <div class="row overflow-hidden" style="max-height: 50px;">
+                      <div class="col-6">
+                        <small>berangkat</small><br />
+                        <small class="font-weight-bold">Aekloba (AKB)</small>
+                      </div>
+                      <div class="col-6">
+                        <small>tiba</small><br />
+                        <small class="font-weight-bold">Argopuro (AGO)</small>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-6">
+                    <svg id="barcodeTiketHistory"></svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="footTiket"></div>
+          </div>`;
+
+    get("#theTiketHistory").innerHTML = loading;
+
+    setTimeout(() => {
+      get("#theTiketHistory").innerHTML = string;
+
+      let widthWindow = window.innerWidth;
+
+      setTimeout(() => {
+        JsBarcode("#barcodeTiketHistory", 160259771197911, {
+          format: "codabar",
+          lineColor: "#000",
+          width: widthWindow < 768 ? 0.9 : 1.5,
+          height: widthWindow < 768 ? 35 : 39,
+          displayValue: true,
+          background: "#f5f5f5",
+          fontSize: "17",
+        });
+      }, 100);
+    }, 3000);
   });
 }
 
 get(".blank").addEventListener("click", function () {
   this.style.opacity = 0;
   this.style.pointerEvents = "none";
+  get("#theTiketHistory").style.opacity = 0;
+  get("#theTiketHistory").style.pointerEvents = "none";
 });
 
 get("#restore").addEventListener("click", function () {
   let href = this.getAttribute("data-id");
   document.location.href = href;
 });
-
-setTimeout(() => {
-  JsBarcode("#barcodeTiketHistory", 160259771197911, {
-    format: "codabar",
-    lineColor: "#000",
-    width: 1.5,
-    height: 39,
-    displayValue: true,
-    background: "#f5f5f5",
-    fontSize: "17",
-  });
-}, 100);
